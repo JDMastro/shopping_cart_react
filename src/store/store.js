575c5ff09f00b0bc4c354b549abeed30b407b6a4
JDMastro <37023992+JDMastro@users.noreply.github.com>
2020-08-25 11:25:28 -0500

    c

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducers';
-
+//
 const composeenhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 let store = createStore(rootReducer, composeenhancers(applyMiddleware(thunk)));
