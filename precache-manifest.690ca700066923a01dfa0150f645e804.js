efb7b5442ee42ffcaa47ea17aea58216260e2459
JDMastro <jdmlmastro@hotmail.com>
2020-09-15 11:55:32 -0500

    Updates

---
+++ precache-manifest.690ca700066923a01dfa0150f645e804.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc5fe3b7d1392f331fab41df21d3023",
+    "url": "/shopping_cart_react/index.html"
+  },
+  {
+    "revision": "3772fcffc20edaed29ea",
+    "url": "/shopping_cart_react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "35c30c2d070cf07ac87c",
+    "url": "/shopping_cart_react/static/css/main.83189603.chunk.css"
+  },
+  {
+    "revision": "3772fcffc20edaed29ea",
+    "url": "/shopping_cart_react/static/js/2.4039b221.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shopping_cart_react/static/js/2.4039b221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c30c2d070cf07ac87c",
+    "url": "/shopping_cart_react/static/js/main.0e39a4f8.chunk.js"
+  },
+  {
+    "revision": "b27d2c95750cf7c76df6",
+    "url": "/shopping_cart_react/static/js/runtime-main.a0edf568.js"
+  }
+]);
